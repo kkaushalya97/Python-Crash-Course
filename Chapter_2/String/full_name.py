ef4c04aa9732a5800ed41e96a3ec20a61c1ad7cc
Kasuni Kaushalya <kkaushalya97@gmail.com>
2025-03-04 14:24:08 +0000

    added string files

---
+++ Chapter_2/String/full_name.py
@@ -0,0 +1,8 @@
+# Using Varibale in string
+first_name = "kasuni"
+second_name = "kaushalya"
+full_name = f"{first_name} {second_name}"  #f-string use to format the python strings varibles value withn the {}
+print(full_name)
+print( f"Hello {full_name}")
+message = f"Hello {full_name.title()}"
+print(message)
---
+++ Chapter_2/String/name.py
@@ -0,0 +1,7 @@
+# change the case in a string with method
+name = "kasuni kaushalya"
+print(name.title())
+
+name = "Kasuni Hewawasamge"
+print(name.lower())
+print(name.upper())
