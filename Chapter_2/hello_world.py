8c548b22fd95e7448dfa5be0f0e658563f58eb64
Kasuni Kaushalya <kkaushalya97@gmail.com>
2025-03-04 14:05:29 +0000

    rename chapter_2 file names

---
+++ Chapter_2/hello_world.py
@@ -0,0 +1,7 @@
+message = "hello python world!."
+print(message)
+
+message = "Hello Python Crash Course World!"
+print(message)
+
+# you can change value of varible always and python always keep track of its current value
