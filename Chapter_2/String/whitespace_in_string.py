fd51891795c378c191e658cf2c841413b3d89d5c
Kasuni Kaushalya <kkaushalya97@gmail.com>
2025-03-05 09:55:23 +0000

    add new file name whitespace

---
+++ Chapter_2/String/whitespace_in_string.py
@@ -0,0 +1,7 @@
+print("python")
+print("\tpython") # adding tab space to the string
+
+print("Languages:\nPython\nC\nJavaScript") #adding new lines to the string
+print("Languages:\n\tPython\n\tC\n\tJavaScript") #adding new line and tab space  to the string
+
+favorite_language = 'Python '
